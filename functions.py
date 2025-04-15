18d0a17f44b59e9d837f304242c48a64fdebabf8
Divyansh Jain <114384181+Divyansh0987@users.noreply.github.com>
2025-04-15 22:25:49 +0800

    Added defaultText parameter

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import customtkinter as ctk
 
-def createEntry(root, text, row, col, paddingX = 0, paddingY = 20):
-    label = ctk.CTkLabel(root, text=text)
+def createEntry(root, text, row, col, paddingX = 0, paddingY = 10, entryText = ""):
+    label = ctk.CTkLabel(root, text=text, font=("Roboto", 16), text_color="#F5F5F5")
     label.grid(row=row, column=col, padx=paddingX, pady=paddingY)
 
-    entry = ctk.CTkEntry(root)
+    entry = ctk.CTkEntry(root, textvariable=ctk.StringVar(value=entryText))
     entry.grid(row=row, column=col+1)
 
     return label, entry
